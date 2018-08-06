659efc60cfbc80aeb137e4153912a0eb43f50de0
Adrien Soepriatna <ASoepriatna@slb.com>
2018-08-06 18:30:00 +0900

    Clean up some template issues

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* Load DOTENVs */
+/* Load DOTENV */
 require('dotenv').config();
 
 var createError = require('http-errors');
@@ -8,13 +8,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+/* View engine setup */
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,20 +24,21 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', apiRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+/* Catch 404 and forward to error handler */
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+/* Error handler */
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  /* Set locals, only providing error in development */
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  /* Render the error page */
   res.status(err.status || 500);
   res.render('error');
 });
